f440b8d68d8e858840d398beb537c6989d0e9e1f
Lluis Mas Ruiz <lmr14@alumnes.udl.cat>
2018-03-12 11:37:16 +0100

    save arezzo files

--- packTub.py
+++ packTub.py
@@ -12,29 +12,44 @@ Created on 02/20/2018
 
 import urllib2
 import bs4
+import sys
 
 class Client(object):
-
+    """
+    @param url: url where we are going to get the information
+    @return: Returns the HTML of the URL
+    """
     def get_web(self, url):
-        f = urllib2.urlopen(url)
+
+        try:
+            f = urllib2.urlopen(url)
+        except Exception as e:
+            print "Something wrong happened with the URL"
+            sys.exit(0)
+
         html = f.read()
         f.close()
         return html
-
+    """
+    @param html: html where we want to find the information.
+    @return: Header that contains the title of the Book.
+    """
     def parser(self, html ):
         soup = bs4.BeautifulSoup(html, "lxml")
         title = soup.find("div", "dotd-title")
 
-        if title == None: return
+        if title == None: return "No Book today"
 
         title_header = title.find("h2")
 
-        if title_header == None: return
-        if title_header.text == None: return
+        if title_header == None: return "No Book today"
+        if title_header.text == None: return "No Book today"
 
         return title_header.text
 
-
+    """
+    @return: Title of the daily book.
+    """
     def run(self):
         url = "https://www.packtpub.com/packt/offers/free-learning?from=block"
         html = self.get_web(url)
@@ -43,9 +58,10 @@ class Client(object):
 
         if book == None: return
 
-        print book.strip()
+        return book.strip()
 
 
 if __name__ == "__main__":
     client = Client()
-    client.run()
+    result = client.run()
+    if result: print result
